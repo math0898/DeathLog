f23b072a2e2440aa5fabc8e9a0eadea00fac1a13
math0898 <25396616+math0898@users.noreply.github.com>
2025-02-23 08:46:51 -0600

    Project Generated

---
+++ src/main/java/io/github/math0898/deathLog/DeathLog.java
@@ -0,0 +1,17 @@
+package io.github.math0898.deathLog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The DeathLog plugin main class.
+ *
+ * @author Sugaku
+ */
+public final class DeathLog extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
